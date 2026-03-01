13d180cba39e924a681bb7f3c6b8c18e919f41fc
drapl0n <drapl0n@protonmail.com>
2026-03-01 22:35:41 +0000

    updated strace-grate-c

--- examples/strace-grate/strace.h
+++ examples/strace-grate/strace.h
@@ -25,7 +25,7 @@ extern syscall_handler_t syscall_handler_table[MAX_SYSCALLS];
 // macro for defining syscall handlers dynamically
 #define DEFINE_HANDLER(name, num, ...)                                          \
     /* function defination for syscall handler */                               \
-    int name##_grate(uint64_t cageid, uint64_t arg1, uint64_t arg1cage,         \
+    int name##_grate(uint64_t cageid, uint64_t arg1, uint64_t arg1cage,          \
                      uint64_t arg2, uint64_t arg2cage,                          \
                      uint64_t arg3, uint64_t arg3cage,                          \
                      uint64_t arg4, uint64_t arg4cage,                          \
@@ -35,15 +35,15 @@ extern syscall_handler_t syscall_handler_table[MAX_SYSCALLS];
         int thiscage = getpid();                                                \
         int types[] = {__VA_ARGS__};                                            \
         int argsnum = sizeof(types) / sizeof(int);                              \
-        uint64_t args[] = {arg1, arg2, arg3, arg4, arg5, arg6};                \
+        uint64_t args[] = {arg1, arg2, arg3, arg4, arg5, arg6};                  \
         uint64_t argcages[] = {arg1cage, arg2cage, arg3cage, arg4cage,          \
                                arg5cage, arg6cage};                             \
                                                                                 \
-        char log_buffer[1024];                                                  \
+        char log_buffer[2048];                                                  \
         int offset = 0;                                                         \
                                                                                 \
         /* log buffer to print syscall with args and ret val */                 \
-        offset += snprintf(log_buffer + offset, 1024 - offset, "%s(", #name);   \
+        offset += snprintf(log_buffer + offset, 1024 - offset, "%s(", #name);    \
                                                                                 \
         for (int i = 0; i < argsnum; i++) {                                     \
             if (i > 0)                                                          \
@@ -72,29 +72,28 @@ extern syscall_handler_t syscall_handler_table[MAX_SYSCALLS];
             }                                                                   \
         }                                                                       \
         offset += snprintf(log_buffer + offset, 1024 - offset, ")");            \
-        									\
-	/* flush log buffer before exit */		    			\
-        int exit_call = (num == 60);               				\
-        if (exit_call) {                    					\
+                                                                                \
+        int is_terminal = (num == 59 || num == 60 || num == 231);               \
+        if (is_terminal) {                                                      \
             fprintf(stderr, "%s\n", log_buffer);                                \
             fflush(stderr);                                                     \
         }                                                                       \
                                                                                 \
         /* forward interposed syscall */                                        \
         int ret = make_threei_call(num, 0,                                      \
-                                   thiscage, arg1cage,                          \
+                                   thiscage, thiscage,                          \
                                    arg1, arg1cage, arg2, arg2cage,              \
                                    arg3, arg3cage, arg4, arg4cage,              \
                                    arg5, arg5cage, arg6, arg6cage, 0);          \
                                                                                 \
-        if (!exit_call) {                                               	\
+        if (!is_terminal) {                                                     \
             fprintf(stderr, "%s = %d\n", log_buffer, ret);                      \
-        } else {                                                                \
-            fprintf(stderr, "%s [failed] = %d\n", log_buffer, ret);             \
+        }  else {                                                               \
+            fprintf(stderr, "%s [failed] = %d\n", log_buffer, ret);              \
         }                                                                       \
                                                                                 \
         /* printing log buffer after return to ensure proper printing */        \
-        /*fprintf(stderr, "%s = %d\n", log_buffer, ret);*/                      \
+        fprintf(stderr, "%s = %d\n", log_buffer, ret);                          \
         return ret;                                                             \
     }                                                                           \
                                                                                 \
