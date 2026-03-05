fe94b1216020df32de8c0dfd3c8544492c4f60cf
drapl0n <drapl0n@protonmail.com>
2026-03-05 23:49:53 +0000

    cleaned printing

--- examples/strace-grate/strace.h
+++ examples/strace-grate/strace.h
@@ -25,13 +25,12 @@ extern syscall_handler_t syscall_handler_table[MAX_SYSCALLS];
 // macro for defining syscall handlers dynamically
 #define DEFINE_HANDLER(name, num, ...)                                          \
     /* function defination for syscall handler */                               \
-    int name##_grate(uint64_t cageid, uint64_t arg1, uint64_t arg1cage,          \
+    int name##_grate(uint64_t cageid, uint64_t arg1, uint64_t arg1cage,         \
                      uint64_t arg2, uint64_t arg2cage,                          \
                      uint64_t arg3, uint64_t arg3cage,                          \
                      uint64_t arg4, uint64_t arg4cage,                          \
                      uint64_t arg5, uint64_t arg5cage,                          \
                      uint64_t arg6, uint64_t arg6cage) {                        \
-                                                                                \
         int thiscage = getpid();                                                \
         int types[] = {__VA_ARGS__};                                            \
         int argsnum = sizeof(types) / sizeof(int);                              \
@@ -43,11 +42,11 @@ extern syscall_handler_t syscall_handler_table[MAX_SYSCALLS];
         int offset = 0;                                                         \
                                                                                 \
         /* log buffer to print syscall with args and ret val */                 \
-        offset += snprintf(log_buffer + offset, 1024 - offset, "%s(", #name);    \
+        offset += snprintf(log_buffer + offset, sizeof(log_buffer) - offset, "%s(", #name); \
                                                                                 \
         for (int i = 0; i < argsnum; i++) {                                     \
             if (i > 0)                                                          \
-                offset += snprintf(log_buffer + offset, 1024 - offset, ", ");   \
+                offset += snprintf(log_buffer + offset, sizeof(log_buffer) - offset, ", "); \
                                                                                 \
             if (types[i] == ARG_STR && args[i] != 0) {                          \
                 char *buf = malloc(256);                                        \
@@ -56,28 +55,23 @@ extern syscall_handler_t syscall_handler_table[MAX_SYSCALLS];
                                             args[i], argcages[i],               \
                                             (uint64_t)buf, thiscage,            \
                                             256, 1);                            \
-                    offset += snprintf(log_buffer + offset, 1024 - offset,      \
+                    offset += snprintf(log_buffer + offset, sizeof(log_buffer) - offset, \
                                        "\"%s\"", buf);                          \
                     free(buf);                                                  \
                 } else {                                                        \
-                    offset += snprintf(log_buffer + offset, 1024 - offset,      \
+                    offset += snprintf(log_buffer + offset, sizeof(log_buffer) - offset, \
                                        "0x%lx", (unsigned long)args[i]);        \
                 }                                                               \
-            } else if (types[i] == ARG_PTR || args[i] > 0xFFFFFF || args[i] == 0) { \
-                offset += snprintf(log_buffer + offset, 1024 - offset,          \
+            } else if (types[i] == ARG_PTR) {                                   \
+                offset += snprintf(log_buffer + offset, sizeof(log_buffer) - offset, \
                                    "0x%lx", (unsigned long)args[i]);            \
             } else {                                                            \
-                offset += snprintf(log_buffer + offset, 1024 - offset,          \
+                offset += snprintf(log_buffer + offset, sizeof(log_buffer) - offset, \
                                    "%ld", (long)args[i]);                       \
             }                                                                   \
         }                                                                       \
-        offset += snprintf(log_buffer + offset, 1024 - offset, ")");            \
                                                                                 \
-        int is_terminal = (num == 59 || num == 60 || num == 231);               \
-        if (is_terminal) {                                                      \
-            fprintf(stderr, "%s\n", log_buffer);                                \
-            fflush(stderr);                                                     \
-        }                                                                       \
+        offset += snprintf(log_buffer + offset, sizeof(log_buffer) - offset, ")"); \
                                                                                 \
         /* forward interposed syscall */                                        \
         int ret = make_threei_call(num, 0,                                      \
@@ -86,18 +80,11 @@ extern syscall_handler_t syscall_handler_table[MAX_SYSCALLS];
                                    arg3, arg3cage, arg4, arg4cage,              \
                                    arg5, arg5cage, arg6, arg6cage, 0);          \
                                                                                 \
-        if (!is_terminal) {                                                     \
-            fprintf(stderr, "%s = %d\n", log_buffer, ret);                      \
-        }  else {                                                               \
-            fprintf(stderr, "%s [failed] = %d\n", log_buffer, ret);              \
-        }                                                                       \
-                                                                                \
-        /* printing log buffer after return to ensure proper printing */        \
         fprintf(stderr, "%s = %d\n", log_buffer, ret);                          \
         return ret;                                                             \
     }                                                                           \
                                                                                 \
-    /* constructor to store handler address in the table */                     \
+    /* constructor to store handler address in the table */                    \
     __attribute__((constructor)) static void register_##name() {                \
         syscall_handler_table[num] = &name##_grate;                             \
     }
