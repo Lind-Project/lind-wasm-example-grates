9b54a507a282d8a0c46cd924e9158bb310357b07
drapl0n <drapl0n@protonmail.com>
2026-03-20 02:35:25 +0000

    use ENV vars instead of hardcoded path

--- examples/seccomp-grate/compile_grate.sh
+++ examples/seccomp-grate/compile_grate.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+LINDFS_ROOT="${LIND_WASM_ROOT}"/lindfs
+
 set -euo pipefail
 
 cd "$(dirname "$0")"
 lind_compile --compile-grate src/seccomp_grate.c src/seccomp.c
-cp src/seccomp-config.ini /home/lind/lind-wasm/lindfs/
+cp src/seccomp-config.ini $LINDFS_ROOT
